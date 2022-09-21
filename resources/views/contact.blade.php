cc1e5e89a4b2c4e24670122a52ef3cf82e999bcd
Abdifatah Hassan <abdi.hassannoor01@gmail.com>
2022-09-21 20:14:36 +0200

    using blades

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,13 @@
+@extends('layouts.app')
+@section('title', 'Contact')
+@section('content')
+
 <form>
-    <input type="text"/>
-    <input type="number"/>
-    <button>Submit</button>
+    <input type="text" name="contact" value="contact"/>
 </form>
+
+@endsection()
+
+@section('footer')
+    <p>This is my footer</p>
+@endsection()
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,1 +1,12 @@
-<h1>{{$id}}</h1>
+@extends('layouts.app')
+@section('title', 'Post')
+@section('header')
+    {{$id}}
+@endsection()
+
+@section('content')
+    <div class="body">1</div>
+    <div class="body">2</div>
+    <div class="body">3</div>
+    <div class="body">4</div>
+@endsection()
