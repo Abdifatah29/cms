8e09bae48dce2f35104cb6500b4ca452a7362660
Abdifatah Hassan <abdi.hassannoor01@gmail.com>
2022-09-24 23:40:10 +0200

    migration column,index and foreign key creations

---
+++ database/migrations/2022_09_24_211734_products.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        /**
+         * Shops table
+         */
+        Schema::dropIfExists('shops');
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('product_id')->unsigned();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate(); 
+
+            // Index
+            $table->index('name');
+            $table->index('created_at');
+            $table->index('product_id');
+
+        });  
+        /**
+         * Products table
+         */
+        Schema::dropIfExists('products');
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('shop_id')->unsigned();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate(); 
+            $table->json('options'); 
+
+            // Indexes
+            $table->index('name');
+            $table->index('created_at');
+            $table->index('shop_id');
+
+            $table->foreign('shop_id')->references('id')->on('shops');
+        });    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('shops');
+    }
+}
